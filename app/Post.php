000e0e3b70df9bb259a124362fc46d7695773617
vicco <victorcs2704@gmail.com>
2020-04-29 20:03:57 -0400

    5. MODEL en una API

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+//    protected $hidden = ['title', 'body'];
+//    protected $appends = ['post_name', 'post_excerpt'];
+//
+//    public function getPostNameAttribute()
+//    {
+//        return strtoupper($this->title);
+//    }
+//
+//    public function getPostExcerptAttribute()
+//    {
+//        return strtoupper(
+//            substr($this->body, 0, 240)
+//        ). ' ...';
+//    }
 }
